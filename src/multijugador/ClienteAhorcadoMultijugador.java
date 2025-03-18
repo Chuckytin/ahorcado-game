c5911b75f45920c57a0a82b0524c19678f40636a
Angel Torcal <130286234+Chuckytin@users.noreply.github.com>
2025-03-18 19:23:16 +0100

    Update ClienteAhorcadoMultijugador.java

--- src/multijugador/ClienteAhorcadoMultijugador.java
+++ src/multijugador/ClienteAhorcadoMultijugador.java
@@ -42,7 +42,7 @@ public class ClienteAhorcadoMultijugador {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(System.err);
         }
     }
 }
